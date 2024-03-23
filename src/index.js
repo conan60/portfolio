c1df3c02e6b68d4be7b173eb1db78d7a6eec55c4
unknown <malek.gorchene@gmail.com>
2024-03-23 05:56:36 +0100

    Dockerfile no audit with install

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './Home';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Home from './Home';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+// ReactDOM.render(<Home />, document.getElementById('root'));
+
+const http = require("http");
+
+const host = 'localhost';
+const port = 3000;
+
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    res.end(`{"message": "This is a JSON response"}`);
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
