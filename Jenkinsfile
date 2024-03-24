3176134d914bf1ca7706a0796f2b102205c1cd42
unknown <malek.gorchene@gmail.com>
2024-03-24 03:41:01 +0100

    Dockerfile no audit with install

--- Jenkinsfile
+++ Jenkinsfile
@@ -8,8 +8,7 @@ pipeline {
         }
         stage('Run container') {
             steps {
-                sh 'docker stop portfolio || true'
-                sh 'docker run -d -p 80:3000 --name portfolio portfolio' 
+                sh 'docker restart portfolio' 
             }
         }
     }
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const port = 3000;
 const requestListener = function (req, res) {
     res.setHeader("Content-Type", "application/json");
     res.writeHead(200);
-    res.end(`{"message": "Nostal aptonia"}`);
+    res.end(`{"message": "Nostal aptonia Malek"}`);
 };
 
 const server = http.createServer(requestListener);
